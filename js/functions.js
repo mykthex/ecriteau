f3e2cfb56567973dfe3f5e9d08edd4f3a6bf880a
Mykael Adam <mykaeladam@gmail.com>
2016-08-02 19:41:13 -0400

    Intégration ++

--- js/functions.js
+++ js/functions.js
@@ -14,5 +14,16 @@ $(document).ready(function() {
         infiniteLoop: true,
         animationSpeed: 300,
     });
-    console.log('ready');
+    $('.map1').customMap({
+        locked: false,
+        htmlMarkers: true,
+        hideHtmlMarkers: false,
+        lat: 46.830396,
+        lng: -71.300340,
+        zoom: 14,
+        styles: [{ "featureType": "administrative", "elementType": "labels.text.fill", "stylers": [{ "color": "#444444" }] }, { "featureType": "landscape", "elementType": "all", "stylers": [{ "color": "#f2f2f2" }] }, { "featureType": "poi", "elementType": "all", "stylers": [{ "visibility": "off" }] }, { "featureType": "road", "elementType": "all", "stylers": [{ "saturation": -100 }, { "lightness": 45 }] }, { "featureType": "road.highway", "elementType": "all", "stylers": [{ "visibility": "simplified" }] }, { "featureType": "road.arterial", "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, { "featureType": "transit", "elementType": "all", "stylers": [{ "visibility": "off" }] }, { "featureType": "water", "elementType": "all", "stylers": [{ "color": "#46bcec" }, { "visibility": "on" }] }],
+        classes: {
+            mapInnerWrapper: 'wrapper',
+        }
+    });
 });
